51846c6d6c3a14c29687deec8ba42d878b5520e9
OstafiichukO <85893003+OstafiichukO@users.noreply.github.com>
2022-02-12 19:22:09 +0200

    refactoring of buttons function
    and comments

--- src/App.js
+++ src/App.js
@@ -5,48 +5,45 @@ import Section from './components/Section';
 import Notification from './components/Notification';
 
 class App extends Component {
+  // состояние 
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-  onGoodIncrement = () => {
-    this.setState(prevState => ({
-      good: prevState.good + 1,
-    }));
+  // варианты отзывов
+  feedbackOptions = ['Good', 'Neutral', 'Bad'];
+
+  //изменяет состояние от нажатия на кнопку отзыва
+  onLeaveFeedback = e => {
+    const option = e.currentTarget.innerText.toLowerCase();
+    return this.setState({ [option]: this.state[option] + 1 });
   };
 
-  onNeutralIncrement = () => {
-    this.setState(prevState => ({
-      neutral: prevState.neutral + 1,
-    }));
-  };
-
-  onBadIncrement = () => {
-    this.setState(prevState => ({
-      bad: prevState.bad + 1,
-    }));
-  };
-
+  //подитывает общее количество отзывов
   countTotalFeedback = () => {
     return Object.values(this.state).reduce((acc, item) => (acc += item), 0);
   };
 
+  // подщитывает процент положительных отзывов от общего количества
   countPositiveFeedbackPercentage = () => {
     return (100 / this.countTotalFeedback()) * this.state.good;
   };
 
+  // делает разметку
   render() {
+    // передаем функции для статистики
     const total = this.countTotalFeedback();
     const positivePercentage = this.countPositiveFeedbackPercentage();
+    
+    // делаем разметку
     return (
       <div>
         <Section title="Please leave feedback">
           <Feedback
-            onGoodIncrement={this.onGoodIncrement}
-            onNeutralIncrement={this.onNeutralIncrement}
-            onBadIncrement={this.onBadIncrement}
+            options={this.feedbackOptions}
+            onLeaveFeedback={this.onLeaveFeedback}
           />
         </Section>
         <Section title="Statistics">
--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,15 +1,11 @@
-const Feedback = ({ onGoodIncrement, onNeutralIncrement, onBadIncrement }) => {
+const Feedback = ({ options, onLeaveFeedback }) => {
   return (
     <div>
-      <button type="button" onClick={onGoodIncrement}>
-        Good
-      </button>
-      <button type="button" onClick={onNeutralIncrement}>
-        Neutral
-      </button>
-      <button type="button" onClick={onBadIncrement}>
-        Bad
-      </button>
+      {options.map(option => (
+        <button type="button" onClick={onLeaveFeedback} key={option}>
+          {option}
+        </button>
+      ))}
     </div>
   );
 };
